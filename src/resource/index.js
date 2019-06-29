498180649aa6bc7d49177d48a2698af77aa22a1f
eduardolagoeiro <eduardolagoeiro@id.uff.br>
2019-06-29 18:20:42 -0300

    fix providers

--- src/resource/index.js
+++ src/resource/index.js
@@ -1,12 +1,8 @@
-import CoursesProvider from './CoursesProvider'
-import ProfessorsProvider from './ProfessorsProvider'
-import TestimoniesProvider from './TestimoniesProvider'
-import UsersProvider from './UsersProvider'
-import TracksProvider from './TracksProvider'
+import GenericProvider from './HttpRequest'
 
 // Give arg to provider to start endpoint with specific path for example = xxx.com/api/person
-export const CoursesService = new CoursesProvider('courses')
-export const ProfessorsService = new ProfessorsProvider('professors')
-export const TestimoniesService = new TestimoniesProvider('testimonies')
-export const UsersService = new UsersProvider('users')
-export const TracksService = new TracksProvider('categories')
+export const CoursesService = new GenericProvider('courses')
+export const ProfessorsService = new GenericProvider('professors')
+export const TestimoniesService = new GenericProvider('testimonies')
+export const UsersService = new GenericProvider('customusers')
+export const TracksService = new GenericProvider('categories')
